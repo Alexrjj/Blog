e3fc995be7094c5e68cd6034b0a40794bcfc2c1f
alexrjj <alexsoares2517@gmail.com>
2020-09-30 16:28:35 -0300

    Update project config

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const moment = require('moment');
 
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }) );
+app.use(bodyParser.json());
+
+// View Engine
+app.set('view engine', 'ejs');
+
+// Static Files
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 // Express Server
 app.listen(3000, () => {
   console.log('Server running on port 3000');
