72c59b878881d831c9b94c871cd3350b282b8fab
alexrjj <alexsoares2517@gmail.com>
2020-09-30 16:18:13 -0300

    Add project setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = Express();
+const bodyParser = require('body-parser');
+const moment = require('moment');
+
+// Express Server
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
